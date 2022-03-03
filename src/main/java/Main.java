63bbf1292fcb7d0ba9818ac431e72edccf013c80
Hooligan <listat25@gmail.com>
2022-03-03 15:01:49 +0200

    almost

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,52 @@
 public class Main {
+
+    static final int size = 10000000;
+    static final int h = size / 2;
+    static float[] arr = new float[size];
+
+
     public static void main(String[] args) {
 
+        firstMethod();
+        secondMethod();
+
+    }
+
+    private static void secondMethod() {
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+        long a = System.currentTimeMillis();
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
+        System.arraycopy(arr, 0, a1, 0, h);
+        System.arraycopy(arr, h, a2, 0, h);
+        Thread t = new Thread(){
+            @Override
+            public void run() {
+                for (int i = 0; i < h; i++) {
+                    a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        };
+        for (int i = 0; i < h; i++) {
+            a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+        System.out.println("Thread method " + (System.currentTimeMillis() - a));
+
+
+    }
+
+    private static void firstMethod() {
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.println(System.currentTimeMillis() - a);
     }
 }
