97a6094761a51cc3546f8d479b68094b20f24ca6
Hooligan <listat25@gmail.com>
2022-04-26 21:15:45 +0300

    java 3 homeWork 4

--- src/main/java/ThreadsPrint.java
+++ src/main/java/ThreadsPrint.java
@@ -4,49 +4,43 @@ public class ThreadsPrint {
 
     public synchronized void printA(){
 //        System.out.println("Method A flag: " + flag);
-        if(flag != 2 && flag != 3){
+        if(flag == 1){
             try {
                 System.out.println("A");
                 flag = 2;
-//                notifyAll();
+                notifyAll();
                 wait();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-        }else{
-            notifyAll();
         }
     }
 
     public synchronized void printB(){
 //        System.out.println("Method B flag: " + flag);
-        if(flag != 1 && flag != 3){
+        if(flag == 2){
             try {
                 System.out.println("B");
-                flag = 3;
+                flag = 1;
+                notifyAll();
                 wait();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-        }else{
-            notifyAll();
-
         }
     }
 
     public synchronized void printC(){
 //        System.out.println("Method C flag: " + flag);
-        if (flag != 2 && flag != 1){
+        if (flag == 3){
             try {
                 System.out.println("C");
                 flag = 1;
-//                notifyAll();
+                notifyAll();
                 wait();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-        }else{
-            notifyAll();
         }
     }
 
